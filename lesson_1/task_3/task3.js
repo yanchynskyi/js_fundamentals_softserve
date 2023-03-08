b485d9edd3624a352e0cf1cd71533c4745415a10
Rostyslav Yanchynskyi <ryanc@softserveinc.com>
2023-03-08 19:13:04 +0200

    task 3 is complete

---
+++ lesson_1/task_3/task3.js
@@ -0,0 +1,18 @@
+//a) оголосіть дві змінні;
+let value1;
+let value2;
+
+//b) запишіть у змінні будь-які значення;
+value1 = "test_value_31";
+value2 = "test_value_2";
+
+//c) виведіть на екран значення змінних;
+console.log(value1);
+console.log(value2);
+
+// d) скопіюйте значення однієї змінної в іншу;
+value2 = value1;
+
+// e) виведіть на екран значення змінних.
+console.log(value1);
+console.log(value2);
