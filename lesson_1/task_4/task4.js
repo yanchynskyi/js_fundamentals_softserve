629b8122f6386e9388a29c1c5796f074fe79d209
Rostyslav Yanchynskyi <ryanc@softserveinc.com>
2023-03-08 19:27:38 +0200

    task 4 is completed

---
+++ lesson_1/task_4/task4.js
@@ -0,0 +1,14 @@
+/*
+Створіть об’єкт з 5-ма наступними властивостями: “String”, “Number”, “Boolean”, “Undefined”, “Null”.
+Значення кожної властивості повинно відповідати відповідному примітивному типу даних.
+*/
+
+let person = {
+  firstName: "John",
+  secondName: "Smith",
+  age: 44,
+  height: 1.92,
+  isAlive: true,
+  nullValue: null,
+  undefinedValue: undefined,
+};
