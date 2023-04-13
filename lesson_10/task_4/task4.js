b95371a2870cc321d9e3f464992170cc081c6f02
Rostyslav Yanchynskyi <ryanc@softserveinc.com>
2023-04-13 20:03:49 +0300

    task4 is complete

---
+++ lesson_10/task_4/task4.js
@@ -0,0 +1,26 @@
+let server = {
+  data: 0,
+  convertToString: function (callback) {
+    callback(() => {
+      return this.data + "";
+    });
+  },
+};
+
+let client = {
+  server: server,
+  result: "",
+  calc: function (data) {
+    this.server.data = data;
+    this.server.convertToString(this.notification());
+  },
+  notification: function () {
+    return (callback) => {
+      this.result = callback();
+    };
+  },
+};
+
+client.calc(123);
+console.log(client.result); // "123"
+console.log(typeof client.result); // "string"
